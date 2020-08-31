6f7190cfc1b26bdf32e44ba136030f35a0bf7bd2
alitoghroli <alitoghroliam@gmail.com>
2020-08-31 14:29:14 +0430

    change

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 let num: string = prompt('Enter a number between 20 and 100: ');
+const n: number = Number(num);
 
-while (num === '' || Number(num) <= 20 || Number(num) >= 100) {
+while (num === '' || n <= 20 || n >= 100) {
   num = prompt('Enter a number between 20 and 100: ');
 }
-let factoriel: number = 1;
-for (let i = 1; i <= Number(num); i++) {
-  factoriel *= i;
-}
 
-console.log(`factoriel ${Number(num)} is : `);
-console.log(factoriel);
-// console.log(BigInt(factoriel));
-console.log(factoriel.toLocaleString());
+import { add } from './addLongs';
+
+const factorial = (n: number): number => {
+  let fact: any = 1;
+
+  for (let i = 2; i <= n; i++) {
+    if (Number.isSafeInteger(fact * i)) {
+      fact = fact * i;
+    } else {
+      //fact = fact + fact + .. i times
+      let factxi = '0'; // this is (fact * i) for us.
+      for (let j = 0; j < i; j++) {
+        factxi = add(factxi, fact.toString());
+      }
+      fact = factxi; // update value of fact before continuing the loop.
+    }
+  }
+  return fact;
+};
+console.log(`factorial ${n} is : `);
+console.log(factorial(n));
